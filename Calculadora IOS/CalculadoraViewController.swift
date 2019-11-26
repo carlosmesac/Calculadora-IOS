6b6936753d58f2edf3f8a7ed30ce0da4bdda5f27
CarlosMesaC <carlosmesac98@gmail.com>
2019-11-26 10:06:34 -0800

    Cuasi Completa

--- Calculadora IOS/CalculadoraViewController.swift
+++ Calculadora IOS/CalculadoraViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class CalculadoraViewController: UIViewController {
 
     var operador:String?
-    var numeroIntroducido:Double = 0
-    var resultado:Double = 0
+    var numeroIntroducido:Int = 0
+    var resultado:Double!
     var numeroPantalla:String?
     var tecla:String?
     var modelo:CalculadoraModelo = CalculadoraModelo()
     var error:Int = 0
+    
     @IBOutlet weak var textoEnPantalla: UILabel!
     
     override func viewDidLoad() {
@@ -30,30 +31,59 @@ class CalculadoraViewController: UIViewController {
     @IBAction func cancelarOperacion() {
         numeroIntroducido = 0
         operador = ""
-        resultado = 0.0
-        textoEnPantalla.text = String(resultado)
+        resultado = nil
+        if(resultado == nil){
+            textoEnPantalla.text = "0"
+        }
     }
     
     @IBAction func cancelarEntrada() {
-    }
     
+        var texto = textoEnPantalla.text
+        guard let nuevoValor = texto?.dropLast() else {return}
+        textoEnPantalla.text = String(nuevoValor)
+        guard let numero = textoEnPantalla.text?.count else {return} //Count cuenta desde 0
+        if(numero == 0){
+            textoEnPantalla.text = "0"
+            numeroIntroducido = 0
+        }else{
+            numeroIntroducido = Int(textoEnPantalla.text ?? "0")!
+        }
+        
+    }
+
     @IBAction func presionaBotonNumero(_ sender: UIButton) {
+        
+        
+        
         guard let tecla = sender.currentTitle else { return }
         if(numeroIntroducido==0){
             textoEnPantalla.text = tecla
+            numeroIntroducido = Int(tecla)!
         } else {
-            guard let anterior = textoEnPantalla.text else {return }
-            textoEnPantalla.text = "\(anterior)\(tecla)"
-
+            
+            numeroIntroducido = numeroIntroducido * 10 + Int(tecla)!
+            textoEnPantalla.text = String(numeroIntroducido)
         }
         
-        
+
         
     }
     
     @IBAction func presionaBotonOperacion(_ sender: UIButton) {
-        var operacion = sender.titleLabel?.text
-        operador = operacion
+        if(resultado == nil){
+            resultado = Double(numeroIntroducido)
+            numeroIntroducido = 0
+            operador = sender.titleLabel?.text
+
+            return
+        }
+        
+        
+        operador = sender.titleLabel?.text
+
+        realizarOperacion()
+
         numeroIntroducido = 0
         
     }
@@ -61,10 +91,15 @@ class CalculadoraViewController: UIViewController {
     func realizarOperacion(){
         switch operador {
         case "+" : resultado = modelo.sumar(resultado, numero2: Double(numeroIntroducido))
+        textoEnPantalla.text = String(resultado)
             break
         case "-" : resultado = modelo.restar(resultado, numero2: Double(numeroIntroducido))
+        textoEnPantalla.text = String(resultado)
+
             break
         case "*" :resultado = modelo.multiplicar(resultado, numero2: Double(numeroIntroducido))
+        textoEnPantalla.text = String(resultado)
+
             break
         case "/" : if(numeroIntroducido == 0){
             resultado = 0.0
@@ -73,18 +108,22 @@ class CalculadoraViewController: UIViewController {
             return
         }else{
             resultado = modelo.dividir(resultado, numero2: Double(numeroIntroducido))
+            textoEnPantalla.text = String(resultado)
+
             error = 0
             }
         case "=" :
-            let temp = Int(self.resultado)
-            let decimales: Double = self.resultado - Double(temp)
+            //let temp = Int(self.resultado)
+            //let decimales: Double = self.resultado - Double(temp)
             
-            if(decimales != 0.0){
-                textoEnPantalla.text = String(format:"%.3f", resultado)
+            //if(decimales != 0.0){
+              //  textoEnPantalla.text = String(format:"%.3f", resultado)
             
-            }else{
-                textoEnPantalla.text = String(format:"%.0f", resultado)
-            }
+           // }else{
+             //   textoEnPantalla.text = String(format:"%.0f", resultado)
+           // }
+            
+            textoEnPantalla.text = String(resultado)
         default:
             error = 2
             return
